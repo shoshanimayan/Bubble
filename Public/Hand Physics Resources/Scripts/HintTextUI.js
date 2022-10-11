758421bfae19e1811a67cd173952c4f4d8c7e1e1
shoshanimayan <mshoshan@uci.edu>
2022-10-10 23:42:50 -0700

    particles and text

--- Public/Hand Physics Resources/Scripts/HintTextUI.js
+++ Public/Hand Physics Resources/Scripts/HintTextUI.js
@@ -5,21 +5,41 @@
 
 // @input Component.Text hintText
 
+var touched =false;
+var tracked=false;
+
 if (script.hintText == undefined) {
     print("ERROR: Make sure to set Hint Text Component");
     script.getSceneObject().destroy();
     return;
 }
 
-script.createEvent("SnapRecordStartEvent").bind(function() {
-    script.hintText.enabled = false;
-});
+
+
+script.createEvent("TouchStartEvent").bind(function () {
+
+    touched=true
+})
 
 script.createEvent("UpdateEvent").bind(function () {
-    if (global.handTracking.getHand() === undefined) {
-        script.hintText.text = "Show your hand";
+   if(!touched)
+    {
+        script.hintText.text = "Tap to blow a bubble";
     }
-    else {
-        script.hintText.text = "Tap to hide or show colliders"
+    else
+    {
+        if(!tracked){
+        if (global.handTracking.getHand() === undefined) {
+                script.hintText.text = "Pop the bubble with your real hand";
+            }
+            else {
+                tracked=true;
+                script.hintText.enabled = false;
+
+            }
+        }
+
     }
+    
+   
 });
--- Public/Scripts/BubbleCollision.js
+++ Public/Scripts/BubbleCollision.js
@@ -1,9 +1,12 @@
 // -----JS CODE-----
+//@input Component.RenderMeshVisual particle
+
 
 var body=script.getSceneObject().getComponent("Physics.BodyComponent");
 var audio = script.getSceneObject().getComponent("Component.AudioComponent");
 var mesh = script.getSceneObject().getComponent("Component.RenderMeshVisual");
 var delayedEvent = script.createEvent("DelayedCallbackEvent");
+script.particle.enabled=false;
 delayedEvent.bind(function(eventData)
 {
     script.getSceneObject().destroy();
@@ -14,6 +17,9 @@ body.onCollisionEnter.add(function (e) {
     audio.play(1);
     mesh.enabled=false;
     body.enabled=false;
+    script.particle.mainPass.externalTimeInput=10;
+    script.particle.enabled=true;
+
     delayedEvent.reset(1);
 
 });
